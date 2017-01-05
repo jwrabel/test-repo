11c118e3101adb5e9ce9d4e07524bdee39dd428c
mgieysztor <michal.gieysztor@gmail.com>
2017-01-05 19:53:30 +0100

    Nowe klasy

---
+++ src/main/java/pl/test/Math.java
@@ -0,0 +1,12 @@
+package pl.test;
+
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-01-05.
+ */
+public class Math {
+    public static List<Factors> getFactors(int number){
+        return null;
+    }
+}
